4f96ace3a92d161a27b13571350f848781acb2ec
rrosztoczy <rrosztoczy@gmail.com>
2019-03-12 15:21:53 -0400

    Got through first etets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,23 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
 
 end
